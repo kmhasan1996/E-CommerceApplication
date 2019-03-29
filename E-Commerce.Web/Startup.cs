6e8fa5bac79ff859f468bfbe4056babb691442ad
Mahidy Hasan <khasan142035@bscse.uiu.ac.bd>
2019-03-29 11:59:48 +0600

    Project Initiated with Login and Registration

---
+++ E-Commerce.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Commerce.Web.Startup))]
+namespace E_Commerce.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
