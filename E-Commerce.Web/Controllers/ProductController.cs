51d841b1f9e2fa90b1eb92c28c3ff193eb03b29f
Mahidy Hasan <khasan142035@bscse.uiu.ac.bd>
2019-03-29 13:16:17 +0600

    Added Category,Product,Shared Controller

---
+++ E-Commerce.Web/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Shared
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProductTable()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(int i)
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(int i)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ E-Commerce.Web/Controllers/SharedController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.Web.Controllers
+{
+    public class SharedController : Controller
+    {
+        public JsonResult UploadImage()
+        {
+            return JsonResult();
+        }
+
+        private JsonResult JsonResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
