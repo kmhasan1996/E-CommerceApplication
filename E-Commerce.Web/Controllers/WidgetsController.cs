6e0646d61e7a5f1a0c2b78d559211e373b8da5f9
Sagor Paul <paulsagor1995@gmail.com>
2019-04-05 02:07:05 +0600

    Added new product on homepage

---
+++ E-Commerce.Web/Controllers/WidgetsController.cs
@@ -0,0 +1,43 @@
+﻿using E_Commerce.Services;
+using E_Commerce.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.Web.Controllers
+{
+    public class WidgetsController : Controller
+    {
+        // GET: Widgets
+         public ActionResult Products(Boolean isLatestProducts, int? CategoryID = 0)
+            {
+                ProductWidgetViewmodel model = new ProductWidgetViewmodel();
+                model.IsLatestProduct = isLatestProducts;
+                if (CategoryID.HasValue && CategoryID.Value > 0)
+                {
+                    model.ByCategory = true;
+
+
+                }
+
+
+                if (isLatestProducts)
+                {
+                    model.Products = ProductService.Instance.GetLatestProducts(4);
+                }
+                else if (CategoryID.HasValue && CategoryID.Value > 0)
+                {
+                    model.Products = ProductService.Instance.GetProductsByCategory(CategoryID.Value, 4);
+                }
+                else
+                {
+                    model.Products = ProductService.Instance.GetEightProducts(8);
+                }
+
+                return PartialView(model);
+            }
+        
+    }
+}
---
+++ E-Commerce.Web/ViewModels/WidgetViewmodels.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce.Web.ViewModels
+{
+    public class ProductWidgetViewmodel
+    {
+        public List<Product> Products { get; set; }
+        public Boolean IsLatestProduct { get; set; }
+        public Boolean ByCategory { get; set; }
+    }
+}
