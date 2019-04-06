4077c30813392a9846fb8f118776f0f9844c58a9
Mahidy Hasan <khasan142035@bscse.uiu.ac.bd>
2019-04-07 01:48:33 +0600

    Created an API to get Products Table

--- E-Commerce.Web/Global.asax.cs
+++ E-Commerce.Web/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+
 
 namespace E_Commerce.Web
 {
@@ -12,10 +14,12 @@ namespace E_Commerce.Web
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
     }
 }
