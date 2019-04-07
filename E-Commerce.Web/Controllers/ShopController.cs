9315c306252a1be1ddc8f3921a9d656522ca745a
Mahidy Hasan <khasan142035@bscse.uiu.ac.bd>
2019-04-07 23:40:14 +0600

    Modified Shop controller

--- E-Commerce.Web/Controllers/ShopController.cs
+++ E-Commerce.Web/Controllers/ShopController.cs
@@ -12,15 +12,39 @@ namespace E_Commerce.Web.Controllers
     {
         public ActionResult Index(string searchTxt, int? minimumPrice, int? maximumPrice, int? categoryID, int? sortBy)
         {
+	    ShopViewmodel model = new ShopViewmodel();
+            model.FeaturedCategories = CategoryService.Instance.GetFeaturedCategory();
+            model.MaximumPrice = ProductService.Instance.GetMaximumPrice();
+            model.Products = ProductService.Instance.SearchProduct(searchTxt, minimumPrice, maximumPrice, categoryID, sortBy);
+            model.SortBy = sortBy;
+            return View(model);
             
         }
         public ActionResult FilterProduct(string searchTxt, int? minimumPrice, int? maximumPrice, int? categoryID, int? sortBy)
         {
+	     FilterProductsViewModel model = new FilterProductsViewModel();
+
+            model.Products = ProductService.Instance.SearchProduct(searchTxt, minimumPrice, maximumPrice, categoryID, sortBy);
+            return PartialView(model);
             
         }
 
         public ActionResult Checkout()
         {
+		CheckoutViewmodel model = new CheckoutViewmodel();
+
+            var CartproductsCookie = Request.Cookies["CartProducts"];
+
+            if (CartproductsCookie != null)
+            {
+                //var productIDs = CartproductsCookie.Value;
+                //var ids = productIDs.Split('-');
+                //List<int> ProductIDs = ids.Select(x => int.Parse(x)).ToList();
+
+                model.CartProductIDs = CartproductsCookie.Value.Split('-').Select(x => int.Parse(x)).ToList();
+                model.CartProducts = ProductService.Instance.GetCartProducts(model.CartProductIDs);
+            }
+            return View();
             
         }
     }
