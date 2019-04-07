60c82e4dff8349a763789c508453dfdbc5b81cbd
MustarySamantha <samanthamustary@gmail.com>
2019-04-07 23:18:48 +0600

    Added shop controller and front-end design for shop

---
+++ E-Commerce.Web/Controllers/ShopController.cs
@@ -0,0 +1,27 @@
+﻿using E_Commerce.Services;
+using E_Commerce.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.Web.Controllers
+{
+    public class ShopController : Controller
+    {
+        public ActionResult Index(string searchTxt, int? minimumPrice, int? maximumPrice, int? categoryID, int? sortBy)
+        {
+            
+        }
+        public ActionResult FilterProduct(string searchTxt, int? minimumPrice, int? maximumPrice, int? categoryID, int? sortBy)
+        {
+            
+        }
+
+        public ActionResult Checkout()
+        {
+            
+        }
+    }
+}
